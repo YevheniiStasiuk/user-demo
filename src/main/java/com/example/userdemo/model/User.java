f3e509054c46bb471818d46837cf0d3c9484d7f9
Yevhenii Stasiuk <yevhenii.stasiuk@gmail.com>
2024-04-26 18:31:49 +0300

    hot fix

--- src/main/java/com/example/userdemo/model/User.java
+++ src/main/java/com/example/userdemo/model/User.java
@@ -2,11 +2,12 @@ package com.example.userdemo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor @AllArgsConstructor
 public class User {
     private long id;
 
